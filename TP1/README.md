32a93c59148f43eaf4d4eb824fa87d46d9a20124
KeimaDT <keima.dt.1412@gmail.com>
2024-09-03 15:13:18 +0200

    Init

---
+++ TP1/README.md
@@ -0,0 +1,7 @@
+# Cours de Vision par ordinateur
+septembre-novembre 2024, ESIEE-IT
+## TP 1
+### Partie A
+à compléter avec vos réponses
+### Partie B
+à compléter avec vos réponses
---
+++ TP1/utils.py
@@ -0,0 +1,3 @@
+##### This file is here to gather all methods that could #####
+##### be usefull throughout the course. This is your #########
+##### custom toolbox #########################################
